c63cf307e7d4c960a5b068aa78d29364949ca350
Isti-k <kiscserkes03@gmail.com>
2024-01-31 11:08:12 +0100

    lin_ker

--- linearis_kereses.py
+++ linearis_kereses.py
@@ -4,11 +4,11 @@ def szamok():
     felso=67
     i=also
     while(i <= felso and not(i % 10 ==0)):
-        i+1
+        i+=1
     
     van = i <= felso
     if(van):
-        print(f"van 0-ra végződő szám:{i}")
+        print(f"van 0-ra végződő szám:" + str(i))
     else:
         print(f"nics 0-ra végződő szám")
 
